3b1bb0689b64e6091c48eaa40f52be719e271133
john <jmwangi288@gmail.com>
2020-09-16 16:52:43 +0300

    added game

--- calc.py
+++ calc.py
@@ -1,8 +1,8 @@
 name1 =input ("input your first name:")
 print("Hello " +name1)
-num1 = float(input("Enter your first number:"))
+num1 = int(input("Enter your first number:"))
 op = input("Enter your operator:")
-num3 = float(input("Enter your second number:"))
+num3 = int(input("Enter your second number:"))
 if op == '*':
     print(num1 * num3)
 elif op == '-':
---
+++ game.py
@@ -0,0 +1,8 @@
+secret_word = "giraffe"
+guess = ""
+
+
+while secret_word != guess:
+    guess = input('not that one! Try again:')
+
+print('Viola! you won')
