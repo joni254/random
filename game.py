093c18f1a15d51958d8b5576b2f3468b434fee6b
john <jmwangi288@gmail.com>
2020-09-16 17:39:12 +0300

    complete

--- game.py
+++ game.py
@@ -1,8 +1,17 @@
 secret_word = "giraffe"
 guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
 
-
-while secret_word != guess:
-    guess = input('not that one! Try again:')
-
-print('Viola! you won')
+while secret_word != guess and not(out_of_guesses):
+    guess_count = guess_count + 1
+    if guess_count < guess_limit:
+        guess = input('not that one! Try again:')
+    else:
+        out_of_guesses = True
+        
+if out_of_guesses:
+    print('Sorry! You LOST.')
+else:
+    print('Viola! you won.')
