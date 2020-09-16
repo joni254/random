927e8522baee633bf5d0c96f6a81f19b02969dd1
john <jmwangi288@gmail.com>
2020-09-16 14:42:53 +0300

    calculator finished

--- calc.py
+++ calc.py
@@ -1,4 +1,15 @@
-print("Hello world")
+name1 =input ("input your first name:")
+print("Hello " +name1)
 num1 = float(input("Enter your first number:"))
 op = input("Enter your operator:")
-num3 = input("Enter your second number:")
+num3 = float(input("Enter your second number:"))
+if op == '*':
+    print(num1 * num3)
+elif op == '-':
+    print(num1 - num3)
+elif op == '+':
+    print(num1 + num3)
+elif op == '/':
+    print(num1/num3)
+else:
+    print('invalid operator')
